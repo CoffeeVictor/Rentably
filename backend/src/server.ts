1f47083e347121ce853fdd8c0396f63d5e9e1dc1
Victor <victor.jgfsilva@gmail.com>
2021-06-13 10:03:05 -0300

    backend: Added CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { routes } from './routes';
+import cors from 'cors';
 
 const app = express();
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
 app.use(express.json());
 app.use(routes);
