c59a95e32eac716f77514d4069f38257174f4ded
Giulia Maia <gmaiaserafim@gmail.com>
2021-06-25 20:36:08 -0300

    Frontend: Added logo to login page

--- frontend/src/views/Login/index.tsx
+++ frontend/src/views/Login/index.tsx
@@ -1,11 +1,17 @@
 import styles from './styles.module.scss';
 import { LoginForm } from './LoginForm';
+import Logo from "./logo.png";
+
 
 export default function Login() {
 	return (
 		<div className={styles.loginPage}>
 			<div className={styles.contentContainer}>
-				<div className={styles.logoContainer}>logo</div>
+				<div className={styles.logoContainer}>	
+				<img src={Logo}></img>
+				<h1>Rentably</h1>
+				<p>Seja bem-vindo ao seu gerenciador de imóveis e aluguéis</p>
+				</div>
 				<div className={styles.formContainer}>
 					<LoginForm />
 				</div>
